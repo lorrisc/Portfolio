4f3de7179318fc462add743e4e235c999961b9e3
noix2kgouu <l.crappier@gmail.com>
2022-10-21 19:55:01 +0200

    projectList

---
+++ website/projects/projectList.js
@@ -0,0 +1,26 @@
+let projectList = {
+    project1: {
+        name: "Transport de sarcophage",
+        date: "Mai 2021",
+        roles: "Dev",
+        techs: "C++, Arduino",
+    },
+    project2: {
+        name: "TrainTravel",
+        date: "Avril 2022",
+        roles: "Design, Dev",
+        techs: "Html, Css, Js, Figma",
+    },
+    project3: {
+        name: "MondialMeteo",
+        date: "Juin 2022",
+        roles: "Dev",
+        techs: "Html, Css, Js",
+    },
+    project4: {
+        name: "TheLegoWar",
+        date: "Oct. 2022",
+        roles: "Dev",
+        techs: "Phaser 3, Js",
+    },
+};
