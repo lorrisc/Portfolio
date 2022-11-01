5dfed97c9998a7c54b90b876f537bddf9b677756
Noix2kgouu <l.crappier@gmail.com>
2022-11-01 21:44:49 +0100

    correction of spelling mistakes

--- projects/projectList.js
+++ projects/projectList.js
@@ -5,8 +5,8 @@ let projectList = [
         roles: "Dev",
         techs: "Phaser 3, Js",
         description: [
-            "Ce petit jeu vidéo jouable sur navigateur à été réalisé avec Phaser 3, un framework JavaScript. Le but du jeu est de récolter les pièces en légo tous en évitant des missiles, des avions et les boules de feu de Wario. Il est possible de tuer Wario en lui tirant dessus. Mais... Wario est très fort est réapparait souvent ! Il est possible de voler, ainsi que de se déplacer.",
-            "Ce petit projet à été développé dans le cadre de mon BUT informatique.",
+            "Ce petit jeu vidéo jouable sur navigateur a été réalisé avec Phaser 3, un framework JavaScript. Le but du jeu est de récolter les pièces en légo tout en évitant des missiles, des avions et les boules de feu de Wario. Il est possible de tuer Wario en lui tirant dessus. Mais... Wario est très fort est réapparait souvent ! Il est possible de voler, ainsi que de se déplacer.",
+            "Ce petit projet a été développé dans le cadre de mon BUT informatique.",
         ],
         linkimage: ["projects/images/phaserLego/gameScreen.jpg"],
         linkproject: [
@@ -20,7 +20,7 @@ let projectList = [
         roles: "Dev",
         techs: "Html, Css, Js",
         description: [
-            "Ce simple projet personnel (inspiré de humanbenchmark.com) m'a permis de développé un premier petit jeu vidéo. Le but est de retrouver les tuiles qui ont été affiché, plus on avance dans les manches, plus le nombre de tuiles augmente.",
+            "Ce simple projet personnel (inspiré de humanbenchmark.com) m'a permis de développer un premier petit jeu vidéo. Le but est de retrouver les tuiles qui ont été affichées, plus on avance dans les manches, plus le nombre de tuiles augmente.",
         ],
         linkimage: ["projects/images/visualMemory/2.PNG", "projects/images/visualMemory/1.PNG"],
         linkproject: [
@@ -34,8 +34,8 @@ let projectList = [
         roles: "Dev",
         techs: "Html, Css, Js",
         description: [
-            "MondialMeteo est un projet personnel. Avec des options de géolocalisation, ou de saisie de ville il affiche la météo du lieu recherché. La météo peut être instantanné, au format 12 heures ou 7 jours. Il est aussi possible de connaître la polution de l'air et les informations  solaire et lunaire.",
-            "Possibilité de sauvegardé les villes recherchées pour les retrouver à la prochaine connexion (gérer avec le local storage). Un historique à également été mis en place de la même façon.",
+            "MondialMeteo est un projet personnel. Avec des options de géolocalisation, ou de saisie de ville, le site affiche la météo du lieu recherché. La météo peut être instantannée, au format 12 heures ou 7 jours. Il est aussi possible de connaître la pollution de l'air et les informations  solaire et lunaire.",
+            "Possibilité de sauvegarder les villes recherchées pour les retrouver à la prochaine connexion (géré avec le local storage). Un historique a également été mis en place de la même façon.",
             "Les différentes informations sont classiquement récupérées via des APIs.",
         ],
         linkimage: ["projects/images/meteo/1home.png", "projects/images/meteo/2hourResult.png", "projects/images/meteo/3hourOpen.png", "projects/images/meteo/4homeSunMoon.png", "projects/images/meteo/5days.png"],
@@ -50,7 +50,7 @@ let projectList = [
         roles: "Design, Dev",
         techs: "Html, Css, Js, Figma",
         description: [
-            "Ce site web permet de visualiser et commander des billets de trains. Il s'inscrit dans le cadre de ma première année de BUT informatique à Valence. Il nous était fourni un serveur pour pouvoir récupérer les différentes informations. Nous pouvions aussi intéragir avec celui-ci pour se connecter à un profil client, ou procéder au paiement de un ou plusieurs billets de train.",
+            "Ce site web permet de visualiser et commander des billets de train. Il s'inscrit dans le cadre de ma première année de BUT informatique à Valence. Un serveur nous était fourni pour pouvoir récupérer les différentes informations. Nous pouvions aussi intéragir avec celui-ci pour se connecter à un profil client, ou procéder au paiement de un ou plusieurs billets de train.",
         ],
         linkimage: ["projects/images/traintravel/1market.jpg", "projects/images/traintravel/2search.jpg", "projects/images/traintravel/3result.jpg", "projects/images/traintravel/4inteMaps.jpg", "projects/images/traintravel/5bestsales.jpg"],
         linkproject: [
@@ -64,9 +64,9 @@ let projectList = [
         roles: "Dev",
         techs: "C++, Arduino",
         description: [
-            "Suite à la découverte de sarcophage en bois dans le Cantal, de nombreux acteurs interviennent et ont pris part dans ce projet pour oeuvrer en faveur de la bonne conservation de ces pièces. En groupe, en fin de terminale (STI2D SIN), nous avons développé un système permettant la gestion de la sécurité des sarcophages lorsqu'ils seront immergés (pour préserver leur états de conservation) au fond d'un lac.",
-            "Nous avons utilisé du matériel Arduino, des capteurs (capteur de température, capteur pH-mètre, capteur de luminosité) pour concevoir un système permettant de surveillé les sarcophages.",
-            "Ce projet a été réalisé en groupe de 4. Deux élèves de STI2D SIN (dont moi) pour la partie informatique, et deux élèves de STI2D ITEC pour concevoir la boite qui accueillera notre système (plongé à 10 mètre sous l'eau, 365 jours par ans). Ce premier important projet nous a également permis de découvrir la gestion de projet.",
+            "Suite à la découverte de sarcophages en bois dans le Cantal, de nombreux acteurs interviennent et prennent part dans ce projet pour oeuvrer en faveur de la bonne conservation de ces pièces. En groupe, en fin de terminale (STI2D SIN), nous avons développé un système permettant la gestion de la sécurité des sarcophages lorsqu'ils seront immergés (pour préserver leur état de conservation) au fond d'un lac.",
+            "Nous avons utilisé du matériel Arduino, des capteurs (capteur de température, capteur pH-mètre, capteur de luminosité) pour concevoir un système permettant de surveiller les sarcophages.",
+            "Ce projet a été réalisé en groupe de quatre. Deux élèves de STI2D SIN (dont moi) pour la partie informatique, et deux élèves de STI2D ITEC pour concevoir la boîte qui accueillera notre système (plongé à 10 mètre sous l'eau, 365 jours par an). Ce premier projet important nous a également permis de découvrir la gestion de projet.",
         ],
         linkimage: ["projects/images/sarcophage/sarcophage.jpg", "projects/images/sarcophage/realisation.jpg"],
         linkproject: [],
